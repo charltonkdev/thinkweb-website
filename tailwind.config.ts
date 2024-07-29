60c134779e1e4b033390c4c5f5bd3e8a93ed6059
Smurfz <charltonk.personal@gmail.com>
2024-07-29 22:30:31 +0800

    fix twcss

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,8 +5,8 @@ import plugin from 'tailwindcss/plugin';
 import tailwindcssAnimate from 'tailwindcss-animate';
 
 const addVariablesForColors = plugin(function ({ addBase, theme }) {
-  const allColors = flattenColorPalette(theme('colors'));
-  const newVars = Object.fromEntries(
+  const allColors = flattenColorPalette(theme('colors')) as Record<string, string>;
+  const newVars: Record<string, string> = Object.fromEntries(
     Object.entries(allColors).map(([key, val]) => [`--${key}`, val])
   );
 
@@ -102,17 +102,17 @@ const config: Config = {
     plugin(function ({ matchUtilities, theme }) {
       matchUtilities(
         {
-          'bg-grid': (value: any) => ({
+          'bg-grid': (value: string) => ({
             backgroundImage: `url("${svgToDataUri(
               `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="32" height="32" fill="none" stroke="${value}"><path d="M0 .5H31.5V32"/></svg>`
             )}")`,
           }),
-          'bg-grid-small': (value: any) => ({
+          'bg-grid-small': (value: string) => ({
             backgroundImage: `url("${svgToDataUri(
               `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="8" height="8" fill="none" stroke="${value}"><path d="M0 .5H31.5V32"/></svg>`
             )}")`,
           }),
-          'bg-dot': (value: any) => ({
+          'bg-dot': (value: string) => ({
             backgroundImage: `url("${svgToDataUri(
               `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="16" height="16" fill="none"><circle fill="${value}" id="pattern-circle" cx="10" cy="10" r="1.6257413380501518"></circle></svg>`
             )}")`,
